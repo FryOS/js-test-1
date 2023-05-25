bf79869ecb542c6e39fa7d528544d5331cefd917
Ospishchev Alexey <alexeyosp@gmail.com>
2023-05-25 23:44:22 +0300

    Вычисление дистанции от точки до точки

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-console.log("hello world");
 
+const addressLat = 2;
+const addressLong = 8;
+const positionLat = 3;
+const positionLong = 7;
+
+const dist = Math.sqrt(((positionLong - positionLat)**2) + (( addressLong - addressLat)**2)); 
+console.log(`Расстояние до точки = ${Math.round(dist)}`);
