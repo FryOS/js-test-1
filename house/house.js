169e44222f4139e4d12e5d43d4caee31484f5621
Ospishchev Alexey <alexeyosp@gmail.com>
2023-05-27 22:41:23 +0300

    переделал все по папкам

--- house/house.js
+++ house/house.js
@@ -1,15 +1,3 @@
-const addressLat = 2;
-const addressLong = 8;
-const positionLat = 3;
-const positionLong = 7;
-
-const dist = Math.sqrt(
-  (positionLong - positionLat) ** 2 + (addressLong - addressLat) ** 2
-);
-console.log(`Расстояние до точки = ${Math.round(dist)}`);
-
-
-
 
 //Задача покупки квартиры
 const sum = 10000;
