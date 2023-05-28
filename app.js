98d1bc6d18f8336e21e35e5309d72ebd30f27934
Ospishchev Alexey <alexeyosp@gmail.com>
2023-05-28 12:07:12 +0300

    Задание с булевой логикой

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-console.log("hello world");
+
+const balance = 1001;
+const bonusBalance = 101;
+const isBanned = false;
+const isExist = false;
+const isSelling = true;
+
+const isCan = (balance > 1000 || bonusBalance > 100) && !isBanned && !isExist && isSelling;
+console.log( `${isCan ? "Yes" : "No"}`);
+
+console.log(true && "PS");
+
+console.log(  "PS" && true);
+
+console.log('12' ?? 'Yes')
 
