94cc434b28dcca35de1aced5e33a95391b3bbed1
Ospishchev Alexey <alexeyosp@gmail.com>
2023-06-04 17:42:40 +0300

    Рефакторинг кода

--- sort-loops/app.js
+++ sort-loops/app.js
@@ -5,14 +5,13 @@ const arr = [1, 40, -5, 10, 0];
 function sortarr(arr) {
   for (let i = 0; i < arr.length - 1; i++) {
     for (let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j] < arr[j + 1]) {
-        // если нужно по убыванию ' > ' поменять на ' < '
-        temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+      if (arr[j] > arr[j + 1]) {
+        // Использовать  ' > ' или ' < ' для разного порядка   
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
     }
   }
 }
+
 sortarr(arr);
-console.log(arr); //[ 40, 10, 1, 0, -5 ]
+console.log(arr); //[ -5, 0, 1, 10, 40 ]
