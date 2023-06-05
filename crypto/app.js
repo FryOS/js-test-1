2487667f7f8dce3f05a61a1f7f20e9bbcf2332d4
Ospishchev Alexey <alexeyosp@gmail.com>
2023-06-05 21:23:16 +0300

    Рефакторинг

--- crypto/app.js
+++ crypto/app.js
@@ -1,13 +1,11 @@
 function encrypt(password) {
-  const left =  password.split("").reverse().join("").slice(0,4);
-  const right =  password.split("").reverse().join("").slice(4,password.length);
-  return [right, left ].join("");
-}
-
-function decrypt(password) {
-  const left =  password.split("").reverse().join("").slice(0,4);
-  const right =  password.split("").reverse().join("").slice(4,password.length);
-  return [left, right].join("");
+  const passArr = [...password];
+  const middle = Math.floor(passArr.length/2);
+  
+  const left =  passArr.join("").slice(0,middle).split("").reverse();
+  const right =  passArr.join("").slice(middle).split(""); 
+  [right[0], right[right.length - 1]] = [right[right.length - 1], right[0]]; 
+  return [...left, ...right].join("");
 }
 
 function check(encryptedPassword, password) {
@@ -15,10 +13,8 @@ function check(encryptedPassword, password) {
 }
 
 const password = 'password';
-const encryptedPassword = encrypt(password);
-console.log(encryptedPassword); // drowssap
+const encryptedPassword = encrypt(password); //ssapdorw
+console.log(encryptedPassword); 
 
 console.log(check(encryptedPassword, password)); // true
 console.log(check(encryptedPassword, 'wrongpassword')); // false
-
-console.log(decrypt(encryptedPassword)); // password
