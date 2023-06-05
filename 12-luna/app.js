84408105fc907ecbfe735e3a4484c95ec30f0095
Ospishchev Alexey <alexeyosp@gmail.com>
2023-06-05 22:13:03 +0300

    Luna function

---
+++ 12-luna/app.js
@@ -0,0 +1,26 @@
+function isValidCardNumber(cardNumber) {
+  // удаляем дефисы из номера карты
+  const carNumberWithoutDashes = cardNumber.replaceAll("-", "");
+
+  // проверяем, что номер карты состоит только из цифр
+  if (isNaN(carNumberWithoutDashes)) {
+    return false;
+  }
+
+  // алгоритм Луна
+  let sum = 0;
+  for (let i = carNumberWithoutDashes.length - 1; i >= 0; i--) {
+    const digit = Number(carNumberWithoutDashes[i]);
+    console.log(digit);
+    if (i % 2 === 0) {
+      const doubleDigit = digit * 2;
+      sum += doubleDigit > 9 ? doubleDigit - 9 : doubleDigit;
+    } else {
+      sum += digit;
+    }
+  }
+  return sum % 10 === 0;
+}
+
+const card = "4561-2612-1234-5464";
+console.log(isValidCardNumber(card));
