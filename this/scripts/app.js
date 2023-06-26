5bca2d64d309ac859a10f9ee5458e8ee9d9d3c69
Ospishchev Alexey <alexeyosp@gmail.com>
2023-06-26 23:08:58 +0300

    Рабочее Добавление привычки и добавление задачи

--- this/scripts/app.js
+++ this/scripts/app.js
@@ -2,7 +2,7 @@
 
 let habbits = [];
 const HABBIT_KEY = "HABBIT_KEY";
-let globalActiveId ;
+let globalActiveId;
 
 /*page*/
 const page = {
@@ -15,6 +15,10 @@ const page = {
   content: {
     daysContainer: document.querySelector(".days"),
     nextDay: document.querySelector(".habbit__day"),
+  },
+  popup: {
+    index: document.getElementById("add-habbit_popup"),
+    fieldIcon: document.querySelector(".popup__form input[name='icon']"),
   },
 };
 
@@ -78,13 +82,13 @@ function rerenderContent(activeHabbit) {
     element.innerHTML = `
       <div class="habbit__day">День ${Number(index) + 1}</div>
       <div class="habbit__comment">${activeHabbit.days[index].comment}</div>
-      <button class="habbit__delete">
+      <button class="habbit__delete" onclick="removeDays(${index})">
           <img src="images/delete.svg" alt="Удалить задачу ${index + 1}">
       </button>    
     `;
     page.content.daysContainer.appendChild(element);
   }
-  page.content.nextDay.innerHTML = `День ${activeHabbit.days.length + 1}`
+  page.content.nextDay.innerHTML = `День ${activeHabbit.days.length + 1}`;
 }
 
 function rerender(activeHabbitId) {
@@ -96,32 +100,114 @@ function rerender(activeHabbitId) {
   rerenderMenu(activeHabbit);
   rerenderHead(activeHabbit);
   rerenderContent(activeHabbit);
-
 }
 
 function addDays(event) {
-  const form = event.target;
   event.preventDefault();
-  const data = new FormData(form);
-  const comment = data.get("comment");
-  form["comment"].classList.remove("error");
-  if(!comment){
-    form["comment"].classList.add("error");
+
+  const data = validateAndGetFormData(event.target, ["comment"]);
+  if (!data) {
+    return;
   }
-  habbits = habbits.map(habbit => {
-    if(habbit.id === globalActiveId){
+  habbits = habbits.map((habbit) => {
+    if (habbit.id === globalActiveId) {
       return {
         ...habbit,
-        days: habbit.days.concat([{comment}])
-      }
+        days: habbit.days.concat([{ comment: data.comment }]),
+      };
     }
     return habbit;
-  })
-  form["comment"].value = "";
+  });
+  resetForm(event.target, ["comment"]);
   rerender(globalActiveId);
-  saveDAta();  
+  saveDAta();
 }
 
+function removeDays(index) {
+  habbits = habbits.map((habbit) => {
+    if (habbit.id === globalActiveId) {
+      habbit.days.splice(index, 1);
+      return {
+        ...habbit,
+        days: habbit.days,
+      };
+    }
+    return habbit;
+  });
+  rerender(globalActiveId);
+  saveDAta();
+}
+
+function togglePopup() {
+  console.log(page.popup.index);
+  if (page.popup.index.classList.contains("cover_hidden")) {
+    page.popup.index.classList.remove("cover_hidden");
+  } else {
+    page.popup.index.classList.add("cover_hidden");
+  }
+}
+
+function validateAndGetFormData(form, fields) {
+  const formData = new FormData(form);
+  const res = {};
+  for (const field of fields) {
+    const fieldValue = formData.get(field);
+    form[field].classList.remove("error");
+    if (!fieldValue) {
+      form[field].classList.add("error");
+    }
+    res[field] = fieldValue;
+  }
+  let isValid = true;
+  for (const field of fields) {
+    if (!res[field]) {
+      isValid = false;
+    }
+  }
+  if (!isValid) {
+    return;
+  }
+  return res;
+}
+
+function resetForm(form, fields) {
+  for (const field of fields) {
+    form[field].value = "";
+  }
+}
+
+function addHabbit(event) {
+  event.preventDefault();
+
+  const data = validateAndGetFormData(event.target, [
+    "name",
+    "icon",
+    "target",
+  ]);
+  if (!data) {
+    return;
+  }
+  const maxId = habbits.reduce((acc, habbit) => acc > habbit.id ? acc : habbit.id, 0);
+  habbits.push({
+    id: maxId + 1,
+    name: data.name,
+    target: data.target,
+    icon: data.icon,
+    days: [],
+  });
+
+  resetForm(event.target, ["name", "target"]);
+  togglePopup();
+  saveDAta();
+  rerender(maxId + 1);
+}
+
+function setIcon(context, icon) {
+  page.popup.fieldIcon.value = icon;
+  const activeIcon = document.querySelector(".icon.icon_active");
+  activeIcon.classList.remove("icon_active");
+  context.classList.add("icon_active");
+}
 
 /*init*/
 (() => {
