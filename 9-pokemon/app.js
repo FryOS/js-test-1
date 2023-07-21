0d6ad1ece4a8a4c8f0171dd8bb5cc79f0ba9a3ed
Ospishchev Alexey <alexeyosp@gmail.com>
2023-07-21 22:42:38 +0300

    выделил код в функцию

--- 9-pokemon/app.js
+++ 9-pokemon/app.js
@@ -1,23 +1,24 @@
-const request = new XMLHttpRequest();
-request.open("GET", "https://pokeapi.co/api/v2/pokemon/ditto");
-request.send();
-
-request.addEventListener("load", function (e) {
-  const { abilities } = JSON.parse(request.responseText);
-  const newUrl = abilities[0].ability.url;
-  console.log(newUrl);
-
-  const request1 = new XMLHttpRequest();
-  request1.open("GET", newUrl);
-  request1.send();
-
-  request1.addEventListener("load", function (e) {
-    const {effect_entries} = JSON.parse(request1.responseText);
-    
-    console.log(effect_entries[1]);
-
-    const {effect} = effect_entries[1];
-    console.log(effect);
-
+function getPokemonData() {
+  const request = new XMLHttpRequest();
+  request.open("GET", "https://pokeapi.co/api/v2/pokemon/ditto");
+  
+  request.send();  
+   
+   request.addEventListener("load", function (e) {
+    const { abilities } = JSON.parse(request.responseText);
+    const newUrl = abilities[0].ability.url;
+      
+    const request1 = new XMLHttpRequest();
+    request1.open("GET", newUrl);
+    request1.send();
+  
+    request1.addEventListener("load", function (e) {
+      const { effect_entries } = JSON.parse(request1.responseText);
+   
+      const { effect } = effect_entries[1];
+      console.log(effect) ;
+    });
   });
-});
+}
+
+getPokemonData();
