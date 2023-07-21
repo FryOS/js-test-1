896493ae6a0eb85f8f5c7f43eacc5adf8596514f
Ospishchev Alexey <alexeyosp@gmail.com>
2023-07-21 22:12:45 +0300

    Вынес фетч запрос в функцию

--- 10-pokemon-promise/app.js
+++ 10-pokemon-promise/app.js
@@ -1,19 +1,23 @@
-//import fetch from "node-fetch";
-//const fetch = require ("node-fetch")
 
-fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-  .then((res) => {
-    if (!res.ok) {
-      throw new Error(`Is error ${res.status}`).message;
-    }
-    return res.json();
-  })
-  .then(({ abilities }) => {
-    console.log(abilities[0].ability.url);
-    return fetch(`${abilities[0].ability.url}`);
-  })
-  .then((res) => res.json())
-  .then(({ effect_entries }) => {
-    console.log(effect_entries[0].effect);
-  })
-  .catch((error) => console.log(error));
+function getPokemonData(url) {
+  fetch(url)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`Is error ${res.status}`).message;
+      }
+      return res.json();
+    })
+    .then(({ abilities }) => {
+      console.log(abilities[0].ability.url);
+      return fetch(`${abilities[0].ability.url}`);
+    })
+    .then((res) => res.json())
+    .then(({ effect_entries }) => {
+      console.log(effect_entries[0].effect);
+    })
+    .catch((error) => console.log(error));
+}
+
+const pokeUrl = "https://pokeapi.co/api/v2/pokemon/ditto";
+
+console.log(getPokemonData(pokeUrl));
