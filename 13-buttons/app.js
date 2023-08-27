861200656163cdc96cbb0f2057ebe86a002e9035
Ospishchev Alexey <alexeyosp@gmail.com>
2023-08-27 22:46:56 +0300

    поправил

--- 13-buttons/app.js
+++ 13-buttons/app.js
@@ -4,17 +4,10 @@ const text = document.querySelector(".text");
 
 let counter = 0;
 
-wrapper.addEventListener("click", function (e) {
-  for (let i = 0; i < buttons.length; i++) {
-    const element = buttons[i];    
+wrapper.addEventListener("click", function (e) {  
     if (e.target.classList.contains("button")) {
       e.target.textContent = "нажата";
     }
-  
-  }
-});
-
-wrapper.addEventListener('click', function() {
-  counter++;
-  text.textContent = counter;
+    counter++;
+    text.textContent = counter;
 });
